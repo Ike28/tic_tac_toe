533fd578ea71bc567ea4975ccc20628d4209f003
Mihai Corocaescu <mihai.corocaescu2@gmail.com>
2023-04-19 12:57:37 +0300

    init

---
+++ android/app/src/main/kotlin/com/google/day_three/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.google.day_three
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const TicTacToeApp());
+}
+
+class TicTacToeApp extends StatelessWidget {
+  const TicTacToeApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Tic Tac Toe',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const TicTacToeMap(title: 'Tic Tac Toe'),
+    );
+  }
+}
+
+class TicTacToeMap extends StatefulWidget {
+  const TicTacToeMap({super.key, required this.title});
+  final String title;
+
+  @override
+  State<TicTacToeMap> createState() => _TicTacToeMapState();
+}
+
+class _TicTacToeMapState extends State<TicTacToeMap> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
