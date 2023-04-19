6df1e25dbb2313ec6870e440ede075cff8748865
Mihai Corocaescu <mihai.corocaescu2@gmail.com>
2023-04-19 14:44:17 +0300

    initial working version

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'pair.dart';
 
 void main() {
   runApp(const TicTacToeApp());
@@ -12,9 +13,8 @@ class TicTacToeApp extends StatelessWidget {
     return MaterialApp(
       title: 'Tic Tac Toe',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const TicTacToeMap(title: 'Tic Tac Toe'),
+          primarySwatch: Colors.yellow, primaryTextTheme: const TextTheme(titleLarge: TextStyle(color: Colors.black))),
+      home: const TicTacToeMap(title: 'tic-tac-toe'),
     );
   }
 }
@@ -28,39 +28,251 @@ class TicTacToeMap extends StatefulWidget {
 }
 
 class _TicTacToeMapState extends State<TicTacToeMap> {
-  int _counter = 0;
+  List<List<Color>> _colorMatrix = List<List<Color>>.generate(3, (_) => List<Color>.filled(3, Colors.white));
+  bool _firstPlayerTurn = true;
+  bool _playAgain = false;
+  bool _gameActive = true;
 
-  void _incrementCounter() {
+  void _setColor(int lineIndex, int columnIndex) {
     setState(() {
-      _counter++;
+      if (_colorMatrix[lineIndex][columnIndex] == Colors.white && _gameActive) {
+        if (_firstPlayerTurn) {
+          _colorMatrix[lineIndex][columnIndex] = Colors.red;
+          _checkGameState(Colors.red);
+        } else {
+          _colorMatrix[lineIndex][columnIndex] = Colors.green;
+          _checkGameState(Colors.green);
+        }
+        _firstPlayerTurn = !_firstPlayerTurn;
+      }
+    });
+  }
+
+  void _checkGameState(final Color playerColor) {
+    List<Pair<int, int>> result = _checkRows(playerColor);
+    if (result.isNotEmpty) {
+      _showWinner(result, playerColor);
+      return;
+    }
+
+    result = _checkColumns(playerColor);
+    if (result.isNotEmpty) {
+      _showWinner(result, playerColor);
+      return;
+    }
+
+    result = _checkDiagonals(playerColor);
+    if (result.isNotEmpty) {
+      _showWinner(result, playerColor);
+      return;
+    }
+    _checkAvailableMoves();
+  }
+
+  List<Pair<int, int>> _checkRows(final Color playerColor) {
+    final List<Pair<int, int>> winningCombination = <Pair<int, int>>[];
+
+    for (int i = 0; i < 3; i++) {
+      for (int j = 0; j < 3; j++) {
+        if (_colorMatrix[i][j] != playerColor) {
+          winningCombination.clear();
+          break;
+        } else {
+          winningCombination.add(Pair<int, int>(i, j));
+        }
+      }
+      if (winningCombination.isNotEmpty) {
+        return winningCombination;
+      }
+    }
+    return winningCombination;
+  }
+
+  List<Pair<int, int>> _checkColumns(final Color playerColor) {
+    final List<Pair<int, int>> winningCombination = <Pair<int, int>>[];
+
+    for (int i = 0; i < 3; i++) {
+      for (int j = 0; j < 3; j++) {
+        if (_colorMatrix[j][i] != playerColor) {
+          winningCombination.clear();
+          break;
+        } else {
+          winningCombination.add(Pair<int, int>(j, i));
+        }
+      }
+      if (winningCombination.isNotEmpty) {
+        return winningCombination;
+      }
+    }
+    return winningCombination;
+  }
+
+  List<Pair<int, int>> _checkDiagonals(final Color playerColor) {
+    final List<Pair<int, int>> winningCombination = <Pair<int, int>>[];
+
+    for (int i = 0; i < 3; i++) {
+      if (_colorMatrix[i][i] != playerColor) {
+        winningCombination.clear();
+        break;
+      } else {
+        winningCombination.add(Pair<int, int>(i, i));
+      }
+    }
+    if (winningCombination.isNotEmpty) {
+      return winningCombination;
+    }
+
+    for (int i = 0; i < 3; i++) {
+      if (_colorMatrix[i][2 - i] != playerColor) {
+        winningCombination.clear();
+        break;
+      } else {
+        winningCombination.add(Pair<int, int>(i, 2 - i));
+      }
+    }
+    return winningCombination;
+  }
+
+  void _checkAvailableMoves() {
+    bool availableSlots = false;
+    for (int i = 0; i < 3; i++) {
+      for (int j = 0; j < 3; j++) {
+        if (_colorMatrix[i][j] == Colors.white) {
+          availableSlots = true;
+        }
+      }
+    }
+    if (!availableSlots) {
+      _endGame();
+    }
+  }
+
+  void _showWinner(final List<Pair<int, int>> winningCombination, Color playerColor) {
+    _colorMatrix = List<List<Color>>.generate(3, (_) => List<Color>.filled(3, Colors.white));
+    for (final Pair<int, int> pair in winningCombination) {
+      _colorMatrix[pair.first][pair.second] = playerColor;
+    }
+    _endGame();
+  }
+
+  void _endGame() {
+    setState(() {
+      _gameActive = false;
+      _playAgain = true;
+    });
+  }
+
+  void _reset() {
+    setState(() {
+      _colorMatrix = List<List<Color>>.generate(3, (_) => List<Color>.filled(3, Colors.white));
+      _firstPlayerTurn = true;
+      _playAgain = false;
+      _gameActive = true;
     });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+        appBar: AppBar(
+          title: Text(widget.title),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
-    );
+        body: Center(
+          child: Column(
+            children: <Widget>[
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  GestureDetector(
+                      onTap: () {
+                        _setColor(0, 0);
+                      },
+                      child: Container(
+                          width: 100,
+                          height: 100,
+                          decoration: BoxDecoration(
+                            border: Border.all(),
+                            color: _colorMatrix[0][0],
+                          ))),
+                  GestureDetector(
+                      onTap: () {
+                        _setColor(0, 1);
+                      },
+                      child: Container(
+                          width: 100,
+                          height: 100,
+                          decoration: BoxDecoration(border: Border.all(), color: _colorMatrix[0][1]))),
+                  GestureDetector(
+                      onTap: () {
+                        _setColor(0, 2);
+                      },
+                      child: Container(
+                          width: 100,
+                          height: 100,
+                          decoration: BoxDecoration(border: Border.all(), color: _colorMatrix[0][2])))
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  GestureDetector(
+                      onTap: () {
+                        _setColor(1, 0);
+                      },
+                      child: Container(
+                          width: 100,
+                          height: 100,
+                          decoration: BoxDecoration(border: Border.all(), color: _colorMatrix[1][0]))),
+                  GestureDetector(
+                      onTap: () {
+                        _setColor(1, 1);
+                      },
+                      child: Container(
+                          width: 100,
+                          height: 100,
+                          decoration: BoxDecoration(border: Border.all(), color: _colorMatrix[1][1]))),
+                  GestureDetector(
+                      onTap: () {
+                        _setColor(1, 2);
+                      },
+                      child: Container(
+                          width: 100,
+                          height: 100,
+                          decoration: BoxDecoration(border: Border.all(), color: _colorMatrix[1][2])))
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  GestureDetector(
+                      onTap: () {
+                        _setColor(2, 0);
+                      },
+                      child: Container(
+                          width: 100,
+                          height: 100,
+                          decoration: BoxDecoration(border: Border.all(), color: _colorMatrix[2][0]))),
+                  GestureDetector(
+                      onTap: () {
+                        _setColor(2, 1);
+                      },
+                      child: Container(
+                          width: 100,
+                          height: 100,
+                          decoration: BoxDecoration(border: Border.all(), color: _colorMatrix[2][1]))),
+                  GestureDetector(
+                      onTap: () {
+                        _setColor(2, 2);
+                      },
+                      child: Container(
+                          width: 100,
+                          height: 100,
+                          decoration: BoxDecoration(border: Border.all(), color: _colorMatrix[2][2])))
+                ],
+              ),
+              Visibility(visible: _playAgain, child: TextButton(onPressed: _reset, child: const Text('PLAY AGAIN')))
+            ],
+          ),
+        ));
   }
 }
